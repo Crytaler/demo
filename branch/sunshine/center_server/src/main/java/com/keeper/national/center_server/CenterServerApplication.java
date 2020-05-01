5b7f394bba0b5c5cba3bd64ba8e8f180b245ee7d
zhen <myz6294369664myz@163.com>
2020-05-01 15:58:35 +0800

    微服务正式版本框架搭建

--- branch/sunshine/center_server/src/main/java/com/keeper/national/center_server/CenterServerApplication.java
+++ branch/sunshine/center_server/src/main/java/com/keeper/national/center_server/CenterServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CenterServerApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(CenterServerApplication.class, args);
     }
